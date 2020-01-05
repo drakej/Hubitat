33909764ea47093706f82883bf60faa227f6eb2d
Jonathan Drake <drakej@drakej.com>
2020-01-04 22:28:02 -0800

    new base for rainbird controller

---
+++ devicetypes/rainbird-controller.groovy
@@ -0,0 +1,108 @@
+/**
+ * RainBird Irrigation Controller
+ * Download: https://github.com/drakej/Hubitat/
+ * Description: This is a device type driver for Hubitat. It communicates with a local Rain Bird API.
+ * 
+ *-------------------------------------------------------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *-------------------------------------------------------------------------------------------------------------------
+ **/
+preferences {
+    input name: "apiHost",        type: "text",   title: "API Host", required: true
+    input name: "logEnable",       type: "bool",   title: "Enable debug logging", defaultValue: true, required: true
+}
+
+metadata {
+    definition (name:      "RainBird Irrigation", 
+		namespace: "drakej", 
+		author:    "Jonathan Drake", 
+		importUrl: "https://raw.githubusercontent.com/drakej/Hubitat/master/devicetypes/rainbird-controller.groovy") {
+		
+        //capability "Switch"
+        capability "Configuration"
+        capability "Refresh"
+        //capability "Valve"
+        //capability "Actuator"
+
+        command 'refresh'
+		
+        attribute "irrigating", "boolean"
+    }
+}
+
+
+/**
+ * Hubitat DTH Lifecycle Functions
+ **/
+def installed() {
+    updated()
+}
+
+def updated(){
+	log.debug "updated"
+    
+}
+
+def refresh() {
+	log.debug "refresh pressed"
+    cleanState()
+    queryControllerInfo()
+}
+
+/**
+ * Event Parsers
+ **/
+def parse(String description) {
+    def msg = parseLanMessage(description)
+    if (msg.status == 200) {
+        if (msg.json) {
+            log.debug msg.json
+            this.state.name = msg.json.Name
+            this.state.serialNumber = msg.json.SerialNumber
+            this.state.model = msg.json.Model
+            this.state.version = msg.json.Version
+        }
+    } 
+}
+
+def networkIdForApp(String appId) {
+    return "${device.deviceNetworkId}-${appId}"    
+}
+
+def appIdForNetworkId(String netId) {
+    return netId.replaceAll(~/.*\-/,"")
+}
+
+private def cleanState() {
+	def keys = this.state.keySet()
+	for (def key : keys) {
+		if (!isStateProperty(key)) {
+			if (logEnable) log.debug("removing ${key}")
+			this.state.remove(key)
+		}
+	}
+}
+
+/**
+ * RainBird API Section
+ **/
+
+def queryControllerInfo() {
+    def result = sendHubCommand(new hubitat.device.HubAction(
+        method: "GET",
+        path: "/controller",
+        headers: [ HOST: "${apiHost}:8080" ]
+    ))
+    
+    httpGet(uri: "http://${apiHost}:8080/irrigation/state") { response ->
+        log.debug response.data
+        sendEvent(name: "irrigating", value: response.data)
+    }
+}
